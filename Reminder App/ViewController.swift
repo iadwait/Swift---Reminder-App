3efd51ec27f9ec321a82374dcc696ec5ef983793
iAdwait <adwaitbarkale@gmail.com>
2020-09-09 09:49:48 +0530

    Initial Commit

---
+++ Reminder App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reminder App
+//
+//  Created by Adwait Barkale on 08/09/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
