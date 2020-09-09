e731ff4d8cf3501c86afe4770b8ed2db31ec05d7
iAdwait <adwaitbarkale@gmail.com>
2020-09-09 09:54:50 +0530

    Files Structured

---
+++ Reminder App/Model/ReminderDataModel.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderDataModel.swift
+//  Reminder App
+//
+//  Created by Adwait Barkale on 09/09/20.
+//  Copyright © 2020 Adwait Barkale. All rights reserved.
+//
+
+import Foundation
+
+
+struct MyReminder
+{
+    var name: String
+    var date: Date
+    var identifier: String
+}
